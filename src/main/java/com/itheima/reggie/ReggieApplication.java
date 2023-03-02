51d4f8d9b3442822d367733d1daf8327789e9bff
xdxiaoran <517639003@qq.com>
2023-03-02 14:57:46 +0800

    功能基本完成，提交初始版本

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,21 @@
+package com.itheima.reggie;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Slf4j
+@ServletComponentScan
+@EnableTransactionManagement
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功...");
+    }
+}
